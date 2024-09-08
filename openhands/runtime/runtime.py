d13a59ec19c2b9558a4af8bfa188c9a7d72cc7e0
RajWorking <rajmaheshwar08@gmail.com>
2024-09-08 17:25:58 +0000

    [Feat] Added FileEditAction to enable edits using diff format.

--- openhands/runtime/runtime.py
+++ openhands/runtime/runtime.py
@@ -13,6 +13,7 @@ from openhands.events.action import (
     BrowseInteractiveAction,
     BrowseURLAction,
     CmdRunAction,
+    FileEditAction,
     FileReadAction,
     FileWriteAction,
     IPythonRunCellAction,
@@ -175,6 +176,10 @@ class Runtime:
 
     @abstractmethod
     def write(self, action: FileWriteAction) -> Observation:
+        pass
+
+    @abstractmethod
+    def edit(self, action: FileEditAction) -> Observation:
         pass
 
     @abstractmethod
